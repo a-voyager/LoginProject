9224b7076cfec0a96e557244f079e4d520a8fff0
wuhaojie <w19961009@126.com>
2015-11-29 11:18:10 +0800

    add UserDao addNewUser

--- src/com/voyager/UserPwdDao.java
+++ src/com/voyager/UserPwdDao.java
@@ -1,5 +1,11 @@
 package com.voyager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * 数据库操作类
  * 
@@ -7,7 +13,67 @@ package com.voyager;
  *
  */
 public class UserPwdDao {
+
+	/**
+	 * 数据库链接对象
+	 */
+	private Connection conn;
+
+	public UserPwdDao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("MySql load failed.");
+			e.printStackTrace();
+		}
+		try {
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/users", "root", "root");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 添加新用户
+	 * 
+	 * @param name
+	 * @param pwd
+	 * @return
+	 */
 	public boolean addNewUser(String name, String pwd) {
+		PreparedStatement statement = null;
+		try {
+			statement = conn
+					.prepareStatement("insert into users (name, pwd) values (?, ?)");
+			statement.setString(1, name);
+			statement.setString(2, pwd);
+			statement.execute();
+		} catch (SQLException e) {
+			return false;
+		} finally {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return true;
 	}
+
+	/**
+	 * 释放资源
+	 */
+	public void dispose() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
+	}
+
 }
