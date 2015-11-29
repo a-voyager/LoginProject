b53bc8c3ce3c9fa9c73b18b95040fe08ae652272
wuhaojie <w19961009@126.com>
2015-11-29 09:01:30 +0800

    init commit.

---
+++ src/com/voyager/Register.java
@@ -0,0 +1,26 @@
+package com.voyager;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+
+	}
+
+}
